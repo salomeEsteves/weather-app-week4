1f6e310720671d86b42f882f62fae4eb6f7d593d
salomeEsteves <r.salome.esteves@gmail.com>
2025-06-27 14:56:45 +0100

    fixed bugs in Degrees.js

--- src/Degrees.js
+++ src/Degrees.js
@@ -1,17 +1,10 @@
 
 import React, { useState } from "react";
+import "./Weather.css"
 
 export default function Degrees(props) {
   const [unit, setUnit] = useState("celsius");
 
-  function toCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function convertToFahrenheit(celsius) {
-    return (celsius * 9) / 5 + 32;
-  }
     return (
       <div className="changeDegrees">
         <span className="temperature">{Math.round(props.celsius)}</span>
